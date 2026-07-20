318819139177b1b296652cb6b1b4932918302dcf
NixxelTzy <nixxeltzy@gmail.com>
2026-07-20 16:59:36 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ if (!fs.existsSync(TEMP_DIR)) fs.mkdirSync(TEMP_DIR, { recursive: true });
 // ---------- MIDDLEWARE ----------
 app.use(cors({ origin: '*' }));
 app.use(express.json({ limit: '50mb' }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
 
 // ---------- RENDER API ----------
 app.post('/api/render', async (req, res) => {
